0b0bf34f15ce19c4141129a1f2145f16a72acc8f
Stas Zaitsev <ststas@yandex.ru>
2023-08-27 15:18:15 +0300

    update env

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const Router = require('./routes');
 const { requestRateLimiter } = require('./utils/requestRateLimiter');
 
-const { PORT = 3000, URI = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT, URI } = process.env;
 
 mongoose.connect(URI, { useNewUrlParser: true });
 
